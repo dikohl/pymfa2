30f8b74b30b503262c0864f4e225db7b69c6179d
Dimitri Kohler <dimitri.kohler@uzh.ch>
2017-07-03 19:19:53 +0200

    clean up - there are no unittests

--- run_tests.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import unittest
-
-testSuite = unittest.TestSuite(unittest.TestLoader().discover("./test"))
-text_runner = unittest.TextTestRunner(None, True, 3).run(testSuite)
